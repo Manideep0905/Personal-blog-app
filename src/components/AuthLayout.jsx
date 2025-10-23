915e889e761978d9080a72e4788430491c4769c7
Manideep0905 <manideepjakkula9@gmail.com>
2025-10-23 22:44:56 +0530

    conclude blog project

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+
+export default function Protected({children, authentication = true}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        // //simpler way by using only authstatus i.e., asking the store about authStatus
+        // if(authStatus === true) {
+        //     navigate("/")
+        // } else if(authStatus === false) {
+        //     navigate("/login")
+        // }
+
+        //complex way i.e., taking the user's authentication status(authentication) and also comparing it with the store(authStatus). Why are we doing this? because we take different scenarios into consideration
+        if(authentication && authStatus !== authentication) {
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication) {
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+    
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+}
